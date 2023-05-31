b1c6f4b4a5c3fa624048ac98091d9c72d5f8cc70
Luis Orozco <89154065+Ucharter@users.noreply.github.com>
2023-05-31 12:02:59 -0500

    Update ScriptsDB.js

--- Model/ScriptsDB.js
+++ Model/ScriptsDB.js
@@ -6,14 +6,7 @@ import { getAuth , createUserWithEmailAndPassword, signInWithEmailAndPassword, s
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBfVMTvk37mEsYBUwRWIEGdY52WUnHRpAo",
-  authDomain: "liquor-store-9c0d9.firebaseapp.com",
-  databaseURL: "https://liquor-store-9c0d9-default-rtdb.firebaseio.com",
-  projectId: "liquor-store-9c0d9",
-  storageBucket: "liquor-store-9c0d9.appspot.com",
-  messagingSenderId: "743260586080",
-  appId: "1:743260586080:web:181791566ebd34a3878305",
-  measurementId: "G-W8YWBF13T7"
+
 };
 
 // Export Firebase
